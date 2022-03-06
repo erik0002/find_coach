8d2085d28583c32e26f8dd5f60200377f9a37346
Yeliazar <erik.ruzhakov@mail.ru>
2022-03-07 01:12:19 +0300

    add store, add data in store and components

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore} from 'vuex';
+
+import coachesModule from './modules/coaches/index';
+
+const store = createStore({
+  modules: {
+    coaches: coachesModule
+  }
+});
+
+export default store;
+
+
