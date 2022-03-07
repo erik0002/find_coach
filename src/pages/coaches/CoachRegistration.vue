942ff581ca94cb8db4429c19028326c30c56a608
Yeliazar <erik.ruzhakov@mail.ru>
2022-03-07 22:56:59 +0300

    add register form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,21 @@
 <template>
-  REGISTER
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+
+<script>
+import CoachForm from '@/components/coaches/CoachForm';
+import BaseCard from '@/components/ui/BaseCard';
+
+export default {
+    components: {
+      BaseCard,
+      CoachForm
+    }
+}
+</script>
