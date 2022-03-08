971584dfb9d9bf0761b98c88a18834c1b91b23cd
Yeliazar <erik.ruzhakov@mail.ru>
2022-03-08 13:21:00 +0300

    send coach requests, caching response data

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <base-dialog :show='!!error' title='An error occurred!' @close='handleError'>
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if='hasRequests'>
+      <base-spinner v-if='isLoading'></base-spinner>
+      <ul v-else-if='hasRequests && !isLoading'>
           <request-item
             v-for='req in receivedRequests'
             :key='req.id'
@@ -21,10 +25,20 @@
 
 <script>
 import RequestItem from '@/components/requests/RequestItem';
+import BaseDialog from '@/components/ui/BaseDialog';
+import BaseSpinner from '@/components/ui/BaseSpinner';
 
 export default {
     components: {
+      BaseSpinner,
+      BaseDialog,
       RequestItem
+    },
+    data() {
+      return {
+        isLoading: false,
+        error: null
+      };
     },
     computed: {
       receivedRequests() {
@@ -33,7 +47,24 @@ export default {
       hasRequests() {
         return this.$store.getters['requests/hasRequests']
       }
-    }
+    },
+    created() {
+      this.loadRequests()
+    },
+    methods: {
+        async loadRequests() {
+          this.isLoading = true;
+          try {
+            await this.$store.dispatch('requests/fetchRequests');
+          } catch(error) {
+            this.error = error.message || 'Something failed!'
+          }
+          this.isLoading = false;
+        },
+        handleError() {
+          this.error = null;
+        }
+      }
 }
 </script>
 
